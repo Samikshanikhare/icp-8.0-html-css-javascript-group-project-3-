dcf5ed7e622924efca68a870d45432b4eacb1c08
patilmahesh007 <patilmahesh4007>
2024-07-17 16:49:52 +0530

    redirect to index.html fixed

--- script/login.js
+++ script/login.js
@@ -1,19 +1,15 @@
+function loginUser(login) {
+    login.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
+    const storedEmail = 'user@gmail.com';
+    const storedPassword = 'user';
 
-    function loginUser(login) {
-        login.preventDefault()
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-
-        const storedEmail = 'user@gmail.com';
-        const storedPassword = 'user';
-
-        if (email === storedEmail && password === storedPassword) {
-            document.getElementById('message').innerText = 'login sucessful';
-            
-            window.location.href = 'index.html';        
-            
-        } else {
-            document.getElementById('message').innerText = 'Invalid email or password';
-        }
+    if (email === storedEmail && password === storedPassword) {
+        document.getElementById('message').innerText = 'Login successful';
+        window.location.href = 'index.html';
+    } else {
+        document.getElementById('message').innerText = 'Invalid email or password';
     }
+}
