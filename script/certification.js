bb6939024eacac35d6d8dc34051409e9a2357eb2
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-07-19 17:45:03 +0530

    add js code

---
+++ script/certification.js
@@ -0,0 +1,103 @@
+function visible(card) {
+    // alert(card)
+
+    if (card === "item1") {
+        let btn = document.getElementById('btn')
+        if (btn.style.display === "none") {
+            btn.style.display = "block"
+        }
+        else {
+            btn.style.display = "none"
+        }
+    }
+    else if (card === "item2") {
+        let btn = document.getElementById('btn1')
+        if (btn.style.display === "none") {
+            btn.style.display = "block"
+        }
+        else {
+            btn.style.display = "none"
+        }
+    }
+
+    else if (card === "item3") {
+        let btn = document.getElementById('btn2')
+        if (btn.style.display === "none") {
+            btn.style.display = "block"
+        }
+        else {
+            btn.style.display = "none"
+        }
+    }
+
+    else if (card === "item4") {
+        let btn = document.getElementById('btn3')
+        if (btn.style.display === "none") {
+            btn.style.display = "block"
+        }
+        else {
+            btn.style.display = "none"
+        }
+    }
+
+
+    else if (card === "item5") {
+        let btn = document.getElementById('btn4')
+        if (btn.style.display === "none") {
+            btn.style.display = "block"
+        }
+        else {
+            btn.style.display = "none"
+        }
+    }
+
+    else if (card === "item6") {
+        let btn = document.getElementById('btn5')
+        if (btn.style.display === "none") {
+            btn.style.display = "block"
+        }
+        else {
+            btn.style.display = "none"
+        }
+    }
+
+    else if (card === "item7") {
+        let btn = document.getElementById('btn6')
+        if (btn.style.display === "none") {
+            btn.style.display = "block"
+        }
+        else {
+            btn.style.display = "none"
+        }
+    }
+
+    else if (card === "item8") {
+        let btn = document.getElementById('btn7')
+        if (btn.style.display === "none") {
+            btn.style.display = "block"
+        }
+        else {
+            btn.style.display = "none"
+        }
+    }
+
+    else if (card === "item9") {
+        let btn = document.getElementById('btn8')
+        if (btn.style.display === "none") {
+            btn.style.display = "block"
+        }
+        else {
+            btn.style.display = "none"
+        }
+    }
+
+    else if (card === "item10") {
+        let btn = document.getElementById('btn9')
+        if (btn.style.display === "none") {
+            btn.style.display = "block"
+        }
+        else {
+            btn.style.display = "none"
+        }
+    }
+}
