1ade6ff3dfb71e0c898887c9b44cf569f31d7b36
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-07-20 20:00:23 +0530

    add changes

--- script/certification.js
+++ script/certification.js
@@ -3,101 +3,101 @@ function visible(card) {
 
     if (card === "item1") {
         let btn = document.getElementById('btn')
-        if (btn.style.display === "none") {
-            btn.style.display = "block"
+        if (btn.style.display != "none") {
+            btn.style.display = "none"
         }
         else {
-            btn.style.display = "none"
+            btn.style.display = "block"
         }
     }
     else if (card === "item2") {
         let btn = document.getElementById('btn1')
-        if (btn.style.display === "none") {
-            btn.style.display = "block"
+        if (btn.style.display != "none") {
+            btn.style.display = "none"
         }
         else {
-            btn.style.display = "none"
+            btn.style.display = "block"
         }
     }
 
     else if (card === "item3") {
         let btn = document.getElementById('btn2')
-        if (btn.style.display === "none") {
-            btn.style.display = "block"
+        if (btn.style.display != "none") {
+            btn.style.display = "none"
         }
         else {
-            btn.style.display = "none"
+            btn.style.display = "block"
         }
     }
 
     else if (card === "item4") {
         let btn = document.getElementById('btn3')
-        if (btn.style.display === "none") {
-            btn.style.display = "block"
+        if (btn.style.display != "none") {
+            btn.style.display = "none"
         }
         else {
-            btn.style.display = "none"
+            btn.style.display = "block"
         }
     }
 
 
     else if (card === "item5") {
         let btn = document.getElementById('btn4')
-        if (btn.style.display === "none") {
-            btn.style.display = "block"
+        if (btn.style.display != "none") {
+            btn.style.display = "none"
         }
         else {
-            btn.style.display = "none"
+            btn.style.display = "block"
         }
     }
 
     else if (card === "item6") {
         let btn = document.getElementById('btn5')
-        if (btn.style.display === "none") {
-            btn.style.display = "block"
+        if (btn.style.display != "none") {
+            btn.style.display = "none"
         }
         else {
-            btn.style.display = "none"
+            btn.style.display = "block"
         }
     }
 
     else if (card === "item7") {
         let btn = document.getElementById('btn6')
-        if (btn.style.display === "none") {
-            btn.style.display = "block"
+        if (btn.style.display != "none") {
+            btn.style.display = "none"
         }
         else {
-            btn.style.display = "none"
+            btn.style.display = "block"
         }
     }
 
     else if (card === "item8") {
         let btn = document.getElementById('btn7')
-        if (btn.style.display === "none") {
-            btn.style.display = "block"
+        if (btn.style.display != "none") {
+            btn.style.display = "none"
         }
         else {
-            btn.style.display = "none"
+            btn.style.display = "block"
         }
     }
 
     else if (card === "item9") {
         let btn = document.getElementById('btn8')
-        if (btn.style.display === "none") {
-            btn.style.display = "block"
+        if (btn.style.display != "none") {
+            btn.style.display = "none"
         }
         else {
-            btn.style.display = "none"
+            btn.style.display = "block"
         }
     }
 
     else if (card === "item10") {
         let btn = document.getElementById('btn9')
-        if (btn.style.display === "none") {
-            btn.style.display = "block"
+        if (btn.style.display != "none") {
+            btn.style.display = "none"
         }
         else {
-            btn.style.display = "none"
+            btn.style.display = "block"
         }
     }
 }
