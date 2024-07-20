96193960ef8da333bde30066930e5208e08b30f1
MansiMali18 <mansimali399@gmail.com>
2024-07-20 11:02:37 +0530

    script added for about us page

---
+++ script/aboutus.js
@@ -0,0 +1,21 @@
+function fun(action)
+        {
+            const inputText=document.getElementById("btn1")
+            const result=document.getElementById("info")
+            if(action == '2020')
+        {
+            result.innerText="Codex, a renowned organization, has been influential in various sectors since its establishment.";
+        }
+        else if(action== '2021')
+        {
+            result.innerText="1000+ students enroll to our course ";
+        }
+        else if(action== '2022')
+        {
+            result.innerText="100+ students got placement in IT companies";
+        }
+        else if(action== '2023')
+        {
+            result.innerText="1000+ students got placement in IT companies";
+        }
+    }
