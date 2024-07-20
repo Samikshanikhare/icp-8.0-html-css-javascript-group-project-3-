82325b38f5d505c8f0737e568606c06e64eb0879
RUCHIIDHAWALE <ruchitadhawale8@gmail.com>
2024-07-20 10:53:46 +0530

    modify file

---
+++ script/course.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.getElementById('body');
+    const checkValue = document.getElementById('checkValue');
+    const menuBar = document.getElementById('menu-bar');
+    const menuOpen = document.querySelector('#')
