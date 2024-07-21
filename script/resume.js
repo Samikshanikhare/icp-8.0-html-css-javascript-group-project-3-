05443fe9a3c74256a49a11789efa1a82bea75a6c
vaishnavishahakar <vaishnavishahakar1@gmail.com>
2024-07-21 14:28:38 +0530

    create a function for mobile number and email address  field

--- script/resume.js
+++ script/resume.js
@@ -2,9 +2,18 @@
 function generateCV() {
     document.getElementById("cv-template").style.display = "block";
     document.getElementById("fullName").innerHTML =
-      document.getElementById("fullNameField").value;
+    document.getElementById("fullNameField").value;
 
 // job title 
 document.getElementById("jobTitle").innerHTML = 
 document.getElementById("jobFiled").value;
+
+// mobile number
+document.getElementById("mobileNumber").innerHTML =
+document.getElementById("mobileNumberField").value;
+
+//email address
+document.getElementById("emailAddress").innerHTML =
+document.getElementById("emailAddressFiled").value;
+
 }
