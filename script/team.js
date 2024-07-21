4ecdc458e6b50b3fd739c1f08cc044e64d44f5f2
Pritinalkar <rutujanalkar07@gmail.com>
2024-07-21 19:18:20 +0530

    moving div added

---
+++ script/team.js
@@ -0,0 +1,8 @@
+
+    let count = 0; 
+    let moving = document.getElementById("moving")
+    document.addEventListener("mousemove",function(e){
+    moving.style.left = `${e.x-100}px`
+    moving.style.top = `${e.y-100}px`
+    })
+            
