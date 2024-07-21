33ba5bfbf94281d191d55d0f3fc85602e2588ef1
Pritinalkar <rutujanalkar07@gmail.com>
2024-07-21 20:06:50 +0530

    footer added

--- script/team.js
+++ script/team.js
@@ -4,5 +4,9 @@
     document.addEventListener("mousemove",function(e){
     moving.style.left = `${e.x-100}px`
     moving.style.top = `${e.y-100}px`
+    setInterval(() => {
+        moving.innerText = count;
+        count++
+    }, 1000);
     })
             
