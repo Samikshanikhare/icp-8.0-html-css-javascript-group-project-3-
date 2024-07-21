a9ab519d99913e3ff87d086de18258d9fabd90fd
vaishnavishahakar <vaishnavishahakar1@gmail.com>
2024-07-21 14:14:46 +0530

    create a function to add job title

--- script/resume.js
+++ script/resume.js
@@ -3,4 +3,8 @@ function generateCV() {
     document.getElementById("cv-template").style.display = "block";
     document.getElementById("fullName").innerHTML =
       document.getElementById("fullNameField").value;
+
+// job title 
+document.getElementById("jobTitle").innerHTML = 
+document.getElementById("jobFiled").value;
 }
