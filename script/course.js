ed10b926e3c1fa7ca22c9b3ca9fc4e78c9c85f6e
RUCHIIDHAWALE <ruchitadhawale8@gmail.com>
2024-07-21 11:53:50 +0530

    modify file

--- script/course.js
+++ script/course.js
@@ -1,6 +1,48 @@
-document.addEventListener('DOMContentLoaded'), () => {
-    const body = document.getElementById('body');
-    const checkValue = document.getElementById('checkValue');
-    const menuBar = document.getElementById('menu-bar');
-    const menuOpen = document.querySelector('#');
-};
+let cart = document.querySelector(".cart");
+let items = document.querySelector(".items");
+
+
+const buyCourses = []
+
+let flag = 0
+
+cart.addEventListener("click", () => {
+    if (flag == 0) {
+    items.style.display = "none"
+        flag = 1;
+    } else if (flag == 1) {
+        items.style.display = "flex"
+        flag = 0;
+    }
+})
+
+
+function addtocart(data) { 
+   buyCourses.push(data)
+}
+
+
+
+let buyitems = ""
+  
+
+
+
+function addtoitems(data) {
+
+    if (!data) {
+        buyitems = `<span> Not yet buy</span>`
+        items.innerHTML = buyitems
+    }
+
+    else {
+        data.forEach(element => {
+            buyitems += `<span>You buy ${element.name} and price ${element.price}</span>`
+        })
+
+        items.innerHTML = buyitems
+    }
+
+}
+
+
