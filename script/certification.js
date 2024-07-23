672db0c12415280212fc73f71371d4c64b8aa587
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-07-23 11:33:29 +0530

    add change in code

--- script/certification.js
+++ script/certification.js
@@ -1,23 +1,120 @@
 function visible(card) {
     // alert(card)
+    switch (card) {
 
-    if (card === "item1") {
-        let btn = document.getElementById('btn')
-        if (btn.style.display === "none") {
-            btn.style.display = "block"
-        }
-        else {
-            btn.style.display = "none"
-        }
+        case 'item1':
+            // alert("item1")
+            let btn1 = document.getElementById('btn')
+            if (btn1.style.display === "none") {
+                btn1.style.display = "block"
+            }
+            else {
+                btn1.style.display = "none"
+            }
+            break;
+
+        case 'item2':
+            // alert("item2")
+            let btn2 = document.getElementById('btn1')
+            if (btn2.style.display === "none") {
+                btn2.style.display = "block"
+            }
+            else {
+                btn2.style.display = "none"
+            }
+            break;
+
+        case 'item3':
+            let btn3 = document.getElementById('btn2')
+            if (btn3.style.display === "none") {
+                btn3.style.display = "block"
+            }
+            else {
+                btn3.style.display = "none"
+            }
+            break;
+
+        case 'item4':
+            let btn4 = document.getElementById('btn3')
+            if (btn4.style.display === "none") {
+                btn4.style.display = "block"
+            }
+            else {
+                btn4.style.display = "none"
+            }
+            break;
+
+        case 'item5':
+            let btn5 = document.getElementById('btn4')
+            if (btn5.style.display === "none") {
+                btn5.style.display = "block"
+            }
+            else {
+                btn5.style.display = "none"
+            }
+            break;
+
+        case 'item6':
+            let btn6 = document.getElementById('btn5')
+            if (btn6.style.display === "none") {
+                btn6.style.display = "block"
+            }
+            else {
+                btn6.style.display = "none"
+            }
+            break;
+
+        case 'item7':
+            let btn7 = document.getElementById('btn6')
+            if (btn7.style.display === "none") {
+                btn7.style.display = "block"
+            }
+            else {
+                btn7.style.display = "none"
+            }
+            break;
+
+        case 'item8':
+            let btn8 = document.getElementById('btn7')
+            if (btn8.style.display === "none") {
+                btn8.style.display = "block"
+            }
+            else {
+                btn8.style.display = "none"
+            }
+            break;
+
+        case 'item9':
+            let btn9 = document.getElementById('btn8')
+            if (btn9.style.display === "none") {
+                btn9.style.display = "block"
+            }
+            else {
+                btn9.style.display = "none"
+            }
+            break;
+
+        case 'item10':
+            let btn10 = document.getElementById('btn9')
+            if (btn10.style.display === "none") {
+                btn10.style.display = "block"
+            }
+            else {
+                btn10.style.display = "none"
+            }
+            break;
+
+    }
+
+
+
+    /*if (card === "item1") {
+        alert("hi")
+        
     }
     else if (card === "item2") {
-        let btn = document.getElementById('btn1')
-        if (btn.style.display === "none") {
-            btn.style.display = "block"
-        }
-        else {
-            btn.style.display = "none"
-        }
+        alert("hi2")
+        
     }
 
     else if (card === "item3") {
@@ -99,5 +196,5 @@ function visible(card) {
         else {
             btn.style.display = "none"
         }
-    }
+    }*/
 }
