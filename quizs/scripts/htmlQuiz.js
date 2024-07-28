7b4e0d0160c16f66da75ef2e41fc00429f054318
nehapansare <nehapansare007@gmail.com>
2024-07-28 11:56:25 +0530

    js id get added

--- quizs/scripts/htmlQuiz.js
+++ quizs/scripts/htmlQuiz.js
@@ -80,3 +80,16 @@ const quizData = [
         correct: "d",
     }
 ];
+
+
+const quiz = document.getElementById("quiz");
+const questionEl = document.getElementById("question");
+const answersEls = document.querySelectorAll(".answer");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+const scoreEl = document.getElementById("score");
+const timerEl = document.getElementById("timer");
+const feedbackEl = document.getElementById("feedback");
